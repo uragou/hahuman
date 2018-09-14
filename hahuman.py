43dc44d247b9f6594a782f4e2fbccaa4d4923a6f
浦郷遼太郎 <jx16041@ns.kogakuin.ac.jp>
2018-09-15 01:07:05 +0900

    木構造うまくいかない　min関数を使おうとしても、中の変数がオブジェクトだとエラー排吐いた　numpy入れた

--- hahuman.py
+++ hahuman.py
@@ -1,4 +1,5 @@
 import os.path
+import numpy
 
 Fname = "test"
 ifp = open(Fname,"rb")
@@ -9,29 +10,99 @@ Htable = []
 Wtable = None
 Hnum = 0
 
-
+#木構造したい
 class HTree:
     def __init__(self,lf,ri,sm):
 
-        self.oya = sm
+        self.oya = None
         self.left  = lf
         self.right = ri
+        self.hi = sm
 
+    def __str__(self): 
+        return str( self.oya )
+
+#2つデータとを取ってそいつを木にしたい。
+#途中で木が複数できたり統合したりする関係でおかしなことになっている
+#奇数のときには一つ目のデータを取っている部分のため、ここでデータが一つしかないかを判断する
+#２つ目の部分ではデータが複数個あるのは確定なので木にしている
+
+#終了条件 元のテーブルが空になって木を入れた配列の長さが１のとき
+#つまりは全てのデータが一つの木に入った時
+#if文のそれぞれは
+"""
+    1. 木がある無い場合
+    1.1 元データが一種しかない場合 : そのまま木にデータにぶち込む　(多分整合性エラー)
+    1.2 元データが複数ある場合    
+    2. 元データが無くて、木がある場合 :　木の中で色々する
+    3. 両方ある場合
+"""
 def MakeTree(TBL):
     print(Htable)
-    TTable = None
-    cnt = 12
-    print(Htable[0][0])
-    print(Htable[0][1])
-    print(Htable[1][0])
-    print(Htable[1][1])
+    Itree = []
+    cnt = 0
+
+    if not Htable:
+        print("not data")
+        return 
     while True:
+        cnt += 1
+        if not(Itree):
+            if len(Htable) == 1:
+                Itree.append(Htable)
+                break
+            else:
+                buf1 = min( Htable )
+                Htable.remove( min( Htable ) )
+        elif not(Htable):
+            if len(Itree) == 1:
+                break
+            buf1 = min( Itree )
+        else:
+            print("Ibuf")
+            print( Itree )
+            print( min( Itree ) )
+            print("Ibuf")
+            Ibuf = min( Itree )
+            buf1 = min( Htable  )
+            
+            if Ibuf[0] < buf1[0]:
+                buf1 = Ibuf
+                Itree.remove( min( Itree ) )
+            else :
+                Htable.remove( min( Htable ) )
+                    
+        if not(Itree):
+            buf2 = min( Htable )
+            Htable.remove( min( Htable ) )
+            Tbuf = HTree(buf1[1] , buf2[1] , buf1[0] + buf2[0] )
+            Itree.append( [  Tbuf.hi , Tbuf ] )
+        elif not(Htable):
+            buf2 = min( Itree )
+            Itree.remove( min( Itree ) )
+            Tbuf = HTree(buf1[1] , buf2[1] , buf1[0] + buf2[0] )
+            Itree.append( [  Tbuf.hi , Tbuf ] )
+        else:
+            Ibuf = min( Itree )
+            buf1 = min( Htable  )
+            if Ibuf[0] < buf1[0]:
+                buf2 = Ibuf
+                Itree.remove( min( Itree ) )
+            else :
+                Htable.remove( min( Htable ) )
+            Tbuf = HTree(buf1[1] , buf2[1] , buf1[0] + buf2[0] )
+            Itree.append( [  Tbuf.hi , Tbuf ] )
+        print("-----------------------------")
+        for lop in range(len(Itree)):
+            print(Itree[lop])
+        print( cnt )
 
-        cnt+=1
-        if cnt == 12:
-            break
+    print("-----------------------------")
+    for lop in range(len(Itree)):
+        print(Itree)
 
-
+#データ取り出し部分
+#１バイトごと取って数を数えている
 for lop in range(siz):
     data = ifp.read(1)
 
@@ -42,12 +113,14 @@ for lop in range(siz):
     else:
         Btable[Bget] = 1
 
+#ソート部分
 #print(Btable)
 #print(len(Btable))
 #print(max(Btable.values()))
 BStable = sorted(Btable.items() , key = lambda x:x[1])
 #print(BStable)
 
+#単純に数えていたバイト列を出現率に変えている
 for lop in range( len( BStable ) ):
     Htable.append( [ Btable[ BStable[lop][0] ] / siz , BStable[lop][0] ] )
     
